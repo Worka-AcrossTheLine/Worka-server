f40c01f4993b4d97585c3962f3d97db8d5d28d26
youngjjjj <>
2020-06-03 23:05:18 +0900

    docker setting & project urls.py update

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3
+
+ENV PYTHONUNBUFFERED=1
+
+RUN apt-get update && apt-get -y install libpq-dev
+
+WORKDIR /app
+ADD . /app
+
+RUN pip3 install -r requirements.txt
+
+
--- worka/settings/comm.py
+++ worka/settings/comm.py
@@ -95,12 +95,14 @@ WSGI_APPLICATION = "worka.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "worka",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
