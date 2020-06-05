98a9db908bc118071b630e376302a73a21b92903
youngjjjj <>
2020-06-05 11:32:01 +0900

    enable black formmating & add python version in Dockerfile

--- worka/settings/dev.py
+++ worka/settings/dev.py
@@ -1,11 +1,11 @@
 from .comm import *
 
 INSTALLED_APPS += [
-    'debug_toolbar',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-INTERNAL_IPS = type(str('c'), (), {'__contains__': lambda *a: True})()
+INTERNAL_IPS = type(str("c"), (), {"__contains__": lambda *a: True})()
