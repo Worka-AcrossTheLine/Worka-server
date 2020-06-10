5deab66317d41d0296ccfacae5a636ba7159c48d
youngjjjj <>
2020-06-10 19:20:26 +0900

    fix settings & requirements

--- worka/settings/comm.py
+++ worka/settings/comm.py
@@ -97,18 +97,18 @@ WSGI_APPLICATION = "worka.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
     # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "worka",
-    #     "USER": "root",
-    #     "PASSWORD": "root",
-    #     "HOST": "localhost",
-    #     "PORT": 5432,
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "worka",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "db",
+        "PORT": 5432,
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
