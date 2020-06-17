2bd2187d01491c2b75c539b3bafe47e53a6d2609
hong <snh1003@naver.com>
2020-06-17 10:45:05 +0900

    add tagsearch, search

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from post import views
-
+from post.views import TagView
 
 router = DefaultRouter()
 router.register("", views.FeedViewSet)
@@ -13,6 +13,7 @@ router.register(
 urlpatterns = [
     path("feed/", include(router.urls)),
     path("all/", views.All.as_view(), name="all"),
+    path("tags/<slug>", views.TagView.as_view(), name="tag"),
     path("like/<uuid:post_id>/", views.Like.as_view(), name="like"),
     path("<uuid:post_id>/likers/", views.Likers.as_view(), name="likers"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,9 +1,13 @@
 from rest_framework import permissions, viewsets, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.filters import SearchFilter
 from .serializers import PostSerializer, CommentSerializer, AuthorSerializer
 from .models import Post, Comment
 from .permissions import IsOwnerOrReadOnly
+
+
+# from django_filters.rest_framework import DjangoFilterBackend
 
 
 class FeedViewSet(viewsets.ModelViewSet):
@@ -38,23 +42,6 @@ class CommentView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         return super().perform_create(serializer)
-
-    # def post(self, request, post_id=None):
-    #     post = Post.objects.get(pk=post_id)
-    #     serializer = CommentSerializer(data=request.data)
-    #
-    #     if serializer.is_valid():
-    #         serializer.save(post=post, author=self.request.user)
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     else:
-    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class ManageComment(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Comment.objects.all()
-#     serializer_class = CommentSerializer
-#     lookup_url_kwarg = "comment_id"
-#     permission_classes = (IsOwnerOrReadOnly,)
 
 
 class Like(APIView):
@@ -94,6 +81,23 @@ class All(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.AllowAny,)
+    filter_backends = [
+        SearchFilter,
+    ]
+    search_fields = ["title", "author__username", "tags__name"]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
+
+class TagView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs.get("slug"))
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
