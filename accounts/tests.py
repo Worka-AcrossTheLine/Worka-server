80af5408f0b89ea962f46cd75fc72dbe341033c1
youngjjjj <>
2020-08-23 14:46:14 +0900

    Add signup testcase

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
+
+
+class AccountsTestCase(APITestCase):
+    def test_signup(self):
+        data = {
+            "username": "testcase",
+            "email": "testcase@test.com",
+            "password": "strong_password",
+        }
+
+        response = self.client.post("/accounts/signup/", data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
